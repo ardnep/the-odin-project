61050a154dbb64683c0aa17502518caa3518f474
Aaradh Nepal <aranepal@gmail.com>
2021-07-01 10:18:40 +0545

    Rename function playRound() -> getRoundWinner() and  Add functions getOverallWinner(), playGame()

--- rock-paper-scissors/scripts/script.js
+++ rock-paper-scissors/scripts/script.js
@@ -7,9 +7,9 @@ function computerPlay() {
     return moveOptions[computerChoice];
 }
 
-function playRound(playerSelection, computerSelection) {
-    // make playerSelection case *in*-sensative
-    playerSelection = playerSelection.toLowerCase();
+function getRoundWinner(playerSelection, computerSelection) {
+    // convert playerSelection to human-readable
+    playerSelection = moveOptions[playerSelection - 1]
 
     switch (true) {
         case (playerSelection === computerSelection):
@@ -26,14 +26,68 @@ function playRound(playerSelection, computerSelection) {
             return "player"; 
         
         default:
-            return "invalid player move";
+            return "invalid";
     } 
 }
 
-let computerSelection = computerPlay();
-console.log("Computer's Move: " + computerSelection);
-let playerSelection;
-while (!playerSelection) {
-    playerSelection = prompt("Choose your move");
+function getOverallWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        return "player";
+    } else if (computerScore > playerScore) {
+        return "computer";
+    } else {
+        return "draw";
+    }
 }
-console.log("Winner: " + playRound(playerSelection, computerSelection));
+
+function playGame(maxRounds) {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // running game for multiple rounds and changing scores accordingly
+    for (let i = 0; i < maxRounds; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection;
+        while (!playerSelection) {
+            playerSelection = prompt("Choose your move:- (1): Rock (2): Paper (3): Scissors");
+        }
+        let roundWinner = getRoundWinner(playerSelection, computerSelection);
+
+        switch (true) {
+            case roundWinner === "player":
+                playerScore++;
+                console.log("You Win This Round!");
+                break;
+            
+            case roundWinner === "computer":
+                computerScore++;
+                console.log("You Lose This Round!");
+                break;
+
+            case roundWinner === "draw":
+                console.log("It's a Draw!");
+                break;
+
+            case roundWinner === "invalid":
+                console.log("Please enter a valid move and try again!");
+                i--;
+        }
+    }
+
+    // displaying overall scores
+    console.log("Your Overall Score: " + playerScore);
+    console.log("Computer's Overall Score: " + computerScore);
+
+    // finding the overall winner
+    let overallWinner = getOverallWinner(playerScore, computerScore); 
+    if (overallWinner === "player") {
+        console.log("Final Result: You won!");
+    } else if(overallWinner === "computer") {
+        console.log("Final Result: You lost!");
+    } else {
+        console.log("Final Result: It's a draw!");
+    }
+}
+
+
+playGame(5);
